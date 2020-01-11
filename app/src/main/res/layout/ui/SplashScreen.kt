5e1b17964b84e9d3f443316769479522578e9771
ajithrameshcr@gmail.com <ajith@xeoscript.com>
2020-01-11 19:18:16 +0530

    Details fragment completed

---
+++ app/src/main/res/layout/ui/SplashScreen.kt
@@ -0,0 +1,56 @@
+package ajithramesh.cr.movielistapp.ui
+
+
+import ajithramesh.cr.movielistapp.R
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SplashScreen : Fragment() {
+
+    private var handler: Handler? = null
+    private var ru: Runnable? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+
+        val v = inflater.inflate(R.layout.fragment_splash_screen, container, false)
+
+
+
+        handler = Handler()
+        ru = object:Runnable {
+             override fun run() {
+                 val homeFrag: HomeFragment  = HomeFragment()
+                 fragmentManager!!.beginTransaction().replace(R.id.main_container,homeFrag,"homeFrag").commit()
+
+            }
+        }
+
+
+
+
+        return v
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler!!.removeCallbacks(ru)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler!!.postDelayed(ru,1800)
+    }
+}
+
